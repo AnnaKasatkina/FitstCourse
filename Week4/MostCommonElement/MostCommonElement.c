e103f882498324c05d73e3a4aefd5e066285746e
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-13 18:49:54 +0300

    Add the read from file

--- Week4/MostCommonElement/MostCommonElement.c
+++ Week4/MostCommonElement/MostCommonElement.c
@@ -6,48 +6,63 @@
 
 #include "Sort.h"
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 
 void print(int* array, int length)
 {
-    for (int i = 0; i < length; i++)
-        printf("%d ", array[i]);
-    printf("\n\n");
+	for (int i = 0; i < length; i++)
+		printf("%d ", array[i]);
+	printf("\n\n");
 }
 
 int mostCommonElement(int* array, int length)
 {
-    int count = 1;
-    int max = 0;
-    int answer = 0;
-    for (int i = 1; i < length; ++i)
-    {
-        if (array[i] == array[i - 1])
-        {
-            count++;
-        }
-        else
-        {
-            if (count >= max)
-            {
-                answer = array[i - 1];
-                max = count;
-                count = 1;
-            }
-            count = 1;
-        }
-    }
-    return answer;
+	int count = 1;
+	int max = 0;
+	int answer = 0;
+	for (int i = 1; i < length; ++i)
+	{
+		if (array[i] == array[i - 1])
+		{
+			count++;
+		}
+		else
+		{
+			if (count >= max)
+			{
+				answer = array[i - 1];
+				max = count;
+				count = 1;
+			}
+			count = 1;
+		}
+	}
+	return answer;
 }
 
 int main(void)
 {
-    int length = 10;
-    int array[] = { 8, 7, 7, 1, 6, 9, 4, 5, 4, 7 };
+	FILE* file = fopen("Array.txt", "r");
 
-    insertionSort(array, 0, length - 1);
+	int array[100] = { 0 };
+	int length = 0;
 
-    printf("%d the most common element in the array: ", mostCommonElement(array, length));
-    print(array, length);
+	while (!feof(file)) 
+	{
+		const int readBytes = fscanf(file, "%d ", &array[length]);
+		if (readBytes < 0) 
+		{
+			break;
+		}
+		++length;
+	}
+	fclose(file);
 
-    return 0;
+	insertionSort(array, 0, length - 1);
+
+	printf("%d the most common element in the array: ", mostCommonElement(array, length));
+	print(array, length);
+
+	return 0;
 }
